89c7816cf28aee648fb8f7c7500fb0f54fb8dc2e
Juan Pablo Rueda <jpruedaort@hotmail.com>
2021-08-10 18:09:03 -0500

    connection pool added

--- src/database.js
+++ src/database.js
@@ -7,13 +7,30 @@ const mysqlConnection = mysql.createConnection({
     database:'JAC_UNIFICADO'
 });
 
-mysqlConnection.connect(function(err){
-    if(err){
-        console.log(err);
-        return;
-    }else{
-        console.log("Db is Connected")
-    }
-});
+function handleDisconnect(conexion_db){
+    connection = mysql.createPool(conexion_db);
+    
+    mysqlConnection.connect(function(err){
+        if(err){
+            console.log(" Error connecting to db ",err);
+            setTimeout(handleDisconnect,2000);
+        }else{
+            console.log("Db is Connected");
+        }
+    });
+    
+    mysqlConnection.on('error',function(err){
+        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+            handleDisconnect();
+        }else{
+            throw err;
+        }
+    });
 
+
+}
+
+
+
+handleDisconnect(mysqlConnection)
 module.exports = mysqlConnection;
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,52 +2,109 @@ const express = require("express");
 const router = express.Router();
 const mysqlConnection = require("../database");
 const bcrypt = require("bcrypt");
-const verify = require('../private');
+const verify = require("../private");
 const e = require("express");
 
 //Informacion del Usuario
-router.post("/complete",verify,(req,res)=>{
-    mysqlConnection.query(`Select cedula, nombres , apellidos, email, rol from afiliados ` ,(err,rows,fields)=>{
-        if(!err){
-            res.json(rows);
-            console.log(rows)
-        }
-        else{
-            console.log(err);
-        }
-    })
+router.post("/complete", verify, (req, res) => {
+	mysqlConnection.query(
+		`Select cedula, nombres , apellidos, email, rol from afiliados `,
+		(err, rows, fields) => {
+			if (!err) {
+				res.json(rows);
+				console.log(rows);
+			} else {
+				console.log(err);
+			}
+		}
+	);
 });
-
-
 
 //Eliminar Usuario
-router.post("/delUser",verify,(req,res)=>{
-    const code = req.body
-    console.log("coodd: ",  code.ced)
-    mysqlConnection.query(`DELETE FROM afiliados where cedula= ${code.ced} ` ,(err,rows,fields)=>{
-        if(!err){
-            res.send(`El registro con cedula ${code.ced} ha sido eliminado`);
-        }
-        else{
-            console.log(err);
-            res.send(`Error en el servidor al eliminar ${code.ced}`)
-        }
-    })
+router.post("/delUser", verify, (req, res) => {
+	const code = req.body;
+	console.log("coodd: ", code.ced);
+	mysqlConnection.query(
+		`DELETE FROM afiliados where cedula= ${code.ced} `,
+		(err, rows, fields) => {
+			if (!err) {
+				res.send(`El registro con cedula ${code.ced} ha sido eliminado`);
+			} else {
+				console.log(err);
+				res.send(`Error en el servidor al eliminar ${code.ced}`);
+			}
+		}
+	);
 });
 
-router.post("/upDate",verify,(req,res)=>{
-    const code = req.body
-    mysqlConnection.query(`UPDATE afiliados set rol="${req.body.rol}" where cedula = ${req.body.ced} `,(err,rows,fields)=>{
-        if(!err){
-            res.send(`El rol de usuario CC: ${req.body.ced} ha sido actualizado correctamente.`);
-        }
-        else{
-            console.log(err);
-            res.send("Error actualizando el rol de Usuario.")
+router.post("/upDate", verify, (req, res) => {
+	const code = req.body;
+	mysqlConnection.query(
+		`UPDATE afiliados set rol="${req.body.rol}" where cedula = ${req.body.ced} `,
+		(err, rows, fields) => {
+			if (!err) {
+				res.send(
+					`El rol de usuario CC: ${req.body.ced} ha sido actualizado correctamente.`
+				);
+			} else {
+				console.log(err);
+				res.send("Error actualizando el rol de Usuario.");
+			}
+		}
+	);
+});
 
-        }
-    })
+router.post("/cominfo", verify, (req, res) => {
+	const code = req.body;
+	const query = `select comite_id from afiliados_comites where cedula= ${req.body.ced} `;
+	mysqlConnection.query(query, (err, rows, fields) => {
+		if (!err) {
+			res.json(rows);
+		} else {
+			console.log(err);
+		}
+	});
+});
 
-})
+router.post("/changecomi", verify, (req, res) => {
+	const code = req.body;
+	const ced = code.ced;
+	const addValue = code.boxValue;
+	const addName = code.boxName;
+	console.log("name: ", code.boxName, "value:  ", code.boxValue);
+	if (addValue === true) {
+		const query = `
+            insert ignore into afiliados_comites (cedula,comite_id) 
+            select ${ced},${addName}
+            where (select count(*) from afiliados_comites where cedula=${ced} and comite_id=${addName})=0;
+        
+        `;
+		mysqlConnection.query(query,(err,rows,fields)=>{
+            if(!err){
+                res.send(`Se agrego exitosamente`)
+            }else{
+                console.log(err);
+                res.send(`Error en el sistema`)
+            }
+        });
+	} else if (addValue === false) {
+        const query = `
+        delete from afiliados_comites where cedula=${ced} and comite_id=${addName};
+        `;
+
+        mysqlConnection.query(query,(err,rows,fields)=>{
+            if(!err){
+                res.send(`Se eliminó exitosamente`)
+            }else{
+                console.log(err);
+                res.send(`Error en el sistema`)
+            }
+        });
+
+
+
+
+	}
+});
 
 module.exports = router;
