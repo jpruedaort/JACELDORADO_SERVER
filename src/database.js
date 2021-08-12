ac825dc2a841aed17ef3bb0ef9b364593ca3f93b
Juan Pablo Rueda <jpruedaort@hotmail.com>
2021-08-12 00:21:28 -0500

    adicion de eventos completado

--- src/database.js
+++ src/database.js
@@ -5,7 +5,8 @@ const mysqlConnection = {
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE
+    database:process.env.DB_DATABASE,
+    multipleStatements: true
 };
 
 function handleDisconnect(conexion_db){
--- src/routes/post.js
+++ src/routes/post.js
@@ -13,12 +13,50 @@ router.post('/user_info',verify,(req,res)=>{
 
 router.post('/new_event',verify,(req,res)=>{
 
-    console.log("cuerpo: ", req.body.form.cominvi)
+    console.log(req.body)
+
+    let comitesLog=req.body.form.cominvi;
+    console.log("comites: ", comitesLog);
+
+    let query = `INSERT INTO EVENTOS (eventos_name, event_owner_email,event_date,event_contenido) values ('${req.body.form.evname}','${req.body.form.evUserEmail}','${req.body.form.evfecha}','${req.body.form.evdescrip}');
+                    set @person_id = LAST_INSERT_ID(); `;
+
     
-    const query = `INSERT INTO EVENTOS 
-                    (event_owner_email,event_date,event_contenido)
-                    values
-                    ('${req.user.email}','${req.body.form.evfecha}','${req.body.form.evdescrip}','${JSON.stringify(req.body.form.cominvi)}')`;
+
+    
+        
+    if(comitesLog.evsalud === true){
+        let concati = 'INSERT INTO eventos_comites (comite_id , event_id) values ( 1 , @person_id); ';
+        query = query + concati;
+    };
+
+    if(comitesLog.evambiente === true){
+        let concati = 'INSERT INTO eventos_comites (comite_id , event_id) values ( 2 , @person_id); ';
+        query = query + concati;
+    };
+
+    if(comitesLog.evdeporte === true){
+        let concati = 'INSERT INTO eventos_comites (comite_id , event_id) values ( 3 , @person_id); ';
+        query = query + concati;
+    };
+
+    if(comitesLog.evjuventud === true){
+        let concati = 'INSERT INTO eventos_comites (comite_id , event_id) values ( 4 , @person_id); ';
+        query = query + concati;
+    };
+
+    if(comitesLog.evgenero === true){
+        let concati = 'INSERT INTO eventos_comites (comite_id , event_id) values ( 5 , @person_id); ';
+        query = query + concati;
+    };
+    
+    console.log("query: " ,query)
+    
+    
+
+    
+    
+
     mysqlConnection.query(query,[],(err, rows, fields)=>{
         if(!err){
             res.json({status:true})
