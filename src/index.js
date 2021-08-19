cba2bdab8c9868335a604fe70e9295f21c8cb179
Juan Pablo Rueda <jpruedaort@hotmail.com>
2021-08-18 21:44:44 -0500

    final 2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 //CORS 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://jaceldoradosfront.herokuapp.com/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://jaceldoradosfront.herokuapp.com"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
